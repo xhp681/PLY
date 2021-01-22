9fa61cb078b6c5b16e32c1695c776d30b6997eb9
Rince King <527883283@qq.com>
2021-01-22 17:14:46 +0800

    ----

---
+++ Rs.PLY/Rs.DataBase/MultipleDatabaseType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs.DataBase
+{
+    public enum MultipleDatabaseType
+    {
+        SQLite,
+        SqlServer,
+        MySql,
+        InMemory,
+        AzureCosmos,
+        Oracle,
+        PostgreSql,
+        Other = 99999,
+        Default = SQLite,
+    }
+}
