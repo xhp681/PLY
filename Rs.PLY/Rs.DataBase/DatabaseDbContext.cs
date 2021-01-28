466d39845bb76ff5f73a397ae5854cf4828df64b
Rince King <527883283@qq.com>
2021-01-28 09:56:23 +0800

    ----

--- Rs.PLY/Rs.DataBase/DatabaseDbContext.cs
+++ Rs.PLY/Rs.DataBase/DatabaseDbContext.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rs.DataBase
 {
-    public abstract class DatabaseDbContext: DbContext
+    public abstract class DatabaseDbContext: DbContext, IMultipleMigrationDbContext
     {
+        protected DatabaseDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var types = modelBuilder.Model.GetEntityTypes().Where(p => typeof(TEntryBase).IsAssignableFrom(p.ClrType));
+            foreach(var entityType in types)
+            {
+                SetGlobalQueryMethodInfo(entityType, modelBuilder);
+            }
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        private void SetGlobalQueryMethodInfo(IMutableEntityType entityType, ModelBuilder modelBuilder)
+        {
+            var dbContextType = this.GetType();
+            dbContextType.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Single(t => t.IsGenericMethod && t.Name == "SetGlobalQuery")
+                .MakeGenericMethod(entityType.ClrType)
+                .Invoke(this, new object[] { modelBuilder });
+        }
     }
 }
---
+++ Rs.PLY/Rs.DataBase/Entiries/TEntryBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs.DataBase
+{
+    [Serializable]
+    public abstract class TEntryBase
+    {
+        /// <summary>
+        /// 唯一标识号
+        /// </summary>
+        public Guid ID { get; set; }
+        /// <summary>
+        /// 数据添加时间
+        /// </summary>
+        public DateTime AddTime { get; set; } = DateTime.Now;
+    }
+}
